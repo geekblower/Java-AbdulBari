71cce7e2737835ede0d1ab02df95c30a3765b6cc
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-07-07 00:04:07 +0530

    Add files via upload

---
+++ CH02/DataTypeDetails.java
@@ -0,0 +1,9 @@
+public class DataTypeDetails {
+    public static void main(String[] args) {
+        int x = 60;
+
+        System.out.println("Binary : " + Integer.toBinaryString(x));
+        System.out.println("Octal : " + Integer.toOctalString(x));
+        System.out.println("Hexadecimal : " + Integer.toHexString(x));
+    }
+}
