81da513f9712da74b6c52f3ebac4987a3b088a5c
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-07-08 23:27:10 +0530

    Delete CH02 directory

--- CH02/DataTypeDetails.java
+++
@@ -1,9 +0,0 @@
-public class DataTypeDetails {
-    public static void main(String[] args) {
-        int x = 60;
-
-        System.out.println("Binary : " + Integer.toBinaryString(x));
-        System.out.println("Octal : " + Integer.toOctalString(x));
-        System.out.println("Hexadecimal : " + Integer.toHexString(x));
-    }
-}
